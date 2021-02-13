0b9f35db523710685ee8800e79361805b163be85
Omkumar <omk7912@gmail.com>
2021-02-14 00:25:53 +0530

    Update settings.py
    add all

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local, You are on production.")
